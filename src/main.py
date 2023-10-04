d7a6b897e3b54df9e77046e0c2103a3e7bb31f8d
MarcusO17 <marcusonggeye@gmail.com>
2023-10-05 03:20:51 +0800

    added loginFunctions

--- src/main.py
+++
@@ -1,3 +0,0 @@
-from model import Login
-
-print(Login.userValidLogin(Login,"sarah@example.com","MyP@ssw0rd"))
--- src/model/Login.py
+++ src/model/Login.py
@@ -8,13 +8,26 @@ class Login:
     def userValidLogin(self,email,password):
         response = requests.get(f'{self.baseURL}users')
         users = json.loads(response.text)
-        for user in users:
-            if email == user['email'] and password == user['password']:
-                return True
-            else:
-                continue
+        if response.status_code == 200:
+            for user in users:
+                if email == user['email'] and password == user['password']:
+                    return True
+                else:
+                    continue
         return False
-
+    
+    def getInfofromEmail(self,email):
+        response = requests.get(f'{self.baseURL}users')
+        if response.status_code == 200:
+            users = json.loads(response.text)
+            for user in users:
+                if email == user['email']:
+                    return {'ID' : user['ID'], 'role': user['role']}
+                else:
+                    continue
+        return False
+    
+ 
 
             
         
