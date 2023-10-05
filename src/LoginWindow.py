c56fb0cd784162f92ec434710629cc2ed9e04f99
kelvinkyy <kelvinkyy03@gmail.com>
2023-10-05 17:37:49 +0800

    Added loginValidation Function

--- src/LoginWindow.py
+++ src/LoginWindow.py
@@ -1,4 +1,5 @@
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QMainWindow, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget
 from PyQt5 import QtCore
 from src.PatientHomepage import PatientHomepage
@@ -15,17 +16,27 @@ class LoginWindow(QWidget):
 
     def initUI(self):
         loginFormLayout = QVBoxLayout()
+        loginFormLayout.setSpacing(0)
+        loginFormLayout.setContentsMargins(20, 20, 20, 20)
+
+        labelFont = QFont()
+        labelFont.setFamily("Poppins")
+        labelFont.setPointSize(12)
 
         self.emailLabel = QLabel("Email:")
+        self.emailLabel.setFont(labelFont)
         self.emailInput = QLineEdit()
 
         self.passwordLabel = QLabel("Password:")
+        self.passwordLabel.setFont(labelFont)
+
         self.passwordInput = QLineEdit()
+        self.passwordInput.setEchoMode(QLineEdit.Password)
 
         self.loginButton = QPushButton("Login")
         self.loginButton.setDefault(True)
 
-        self.loginButton.clicked.connect(self.login)
+        self.loginButton.clicked.connect(self.loginValidation)
 
         loginFormLayout.addWidget(self.emailLabel)
         loginFormLayout.addWidget(self.emailInput)
@@ -40,6 +51,21 @@ class LoginWindow(QWidget):
         self.patientHomepage = PatientHomepage()
         self.patientHomepage.show()
         self.close()
+
+
+    def loginValidation(self):
+        email = self.emailInput.text()
+        password = self.passwordInput.text()
+
+        #backend
+        tempemail = ""
+        temppassword = ""
+
+        if email == tempemail and temppassword == temppassword:
+            print("login success")
+            self.login()
+        else:
+            print("login failed")
 
 
 
--- src/PatientHomepage.py
+++ src/PatientHomepage.py
@@ -1,19 +1,33 @@
+from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QLabel, QPushButton
 
 
 class PatientHomepage(QMainWindow):
     def __init__(self):
         super().__init__()
-
+        self.setWindowTitle("Homepage")
+        self.setFixedWidth(1280)
+        self.setFixedHeight(720)
         self.init_ui()
 
     def init_ui(self):
         self.setWindowTitle("Homepage")
         self.setGeometry(100, 100, 400, 300)
 
-        layout = QVBoxLayout()
+        headerFont = QFont()
+        headerFont.setFamily("Poppins")
+        headerFont.setBold(True)
+        headerFont.setPointSize(48)
 
-        welcome_label = QLabel("Welcome!")
-        layout.addWidget(welcome_label)
+        patientHomepageLayout = QVBoxLayout()
 
-        self.setCentralWidget(layout)
+        title = QLabel("Welcome!")
+        title.setFont(headerFont)
+        patientHomepageLayout.addWidget(title)
+
+        central_widget = QWidget()
+        central_widget.setLayout(patientHomepageLayout)
+        self.setCentralWidget(central_widget)
+
+
+
