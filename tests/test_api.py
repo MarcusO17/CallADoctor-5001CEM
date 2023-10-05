27d2e3b1327f34babd499150c245f1c1401925f6
MarcusO17 <marcusonggeye@gmail.com>
2023-10-05 18:39:11 +0800

    added get_users tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,8 +1,25 @@
 import requests
+import pytest
 
-BASE_URL = "http://127.0.0.1:5000/"
+BASE_URL = "http://127.0.0.1:5000"
 
 def test_index():
-    response = requests.get(BASE_URL)
-    assert response.status_code == 200
+    try:
+        #Requesting index
+        response = requests.get(BASE_URL + "/") 
+        #Test if index is OK
+        assert response.status_code == 200
+
+    except requests.exceptions.ConnectionError as e:
+        assert False, f"Connection Closed : {e}"
+
+def test_get_users():
+    try:
+        #Requesting index
+        response = requests.get(BASE_URL + "/users") 
+        #Test if index is OK
+        assert response.status_code == 200
+
+    except requests.exceptions.RequestException as e:
+        assert False, f"Failure : {e}"
 
