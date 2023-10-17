b95b89e3b6ce4106b41f55cb28243cd483b0943b
MarcusO17 <marcusonggeye@gmail.com>
2023-10-17 17:29:21 +0800

    retry mocking

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,25 +1,9 @@
 import requests
-import unittest
-from unittest.mock import Mock, patch
+import pytest
 
-
-def getPatients():
-    response = requests.get('http://127.0.0.1:5000/patients')
-    return response.json
-
-class testAPI(unittest.TestCase):
-    URL = 'http://127.0.0.1:5000'
-
-    def test_index(self):
-        response  = requests.get(f'{self.URL}/')
-        assert response.status_code == 200
-
-
-    @patch('requests.get')
-    def test_get_patient(self,mockGet):
-        mockResponse = Mock()
-
-        mockResult = {
+@pytest.fixture
+def mockPatients():
+    return {
                 "patientID": "12345",
                 "patientName": "John Doe",
                 "address": "123 Main St, City, Country",
@@ -27,14 +11,16 @@ class testAPI(unittest.TestCase):
                 "bloodType": "A+",
                 "race": "Caucasian"
             }
-        
-        mockResponse.json.return_value = mockResult 
-        mockGet.return_value = mockResponse
 
-        result = getPatients()
-        mockGet.assert_called_with(f'{self.URL}/patients')
-       
-        self.assertEqual(result, mockResult)
+def test_getPatients(mocker,mockPatients):
+    mocker.patch(
+        'requests.get("http://127.0.0.1:5000/patients")'
+        mockValue = mockPatients
+    )expected = (
+     response = requests.get("http://127.0.0.1:5000/patients")
+    )
+    ss
+
 
 if __name__ == '__main__':
     unittest.main()
