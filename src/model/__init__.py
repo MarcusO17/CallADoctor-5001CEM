2f490d57e5889c4c38707bb6b8410f779778e672
kelvinkyy <kelvinkyy03@gmail.com>
2023-10-18 00:46:35 +0800

    Added Doctor Model

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -2,6 +2,7 @@ from .Login import Login
 from .Clinic import Clinic
 from .Registration import Registration
 from .Appointment import Appointment
+from .Doctor import Doctor
 
-__all__ = ['Login', 'Registration','Clinic','Appointment']
+__all__ = ['Login', 'Registration','Clinic','Appointment','Doctor']
 
