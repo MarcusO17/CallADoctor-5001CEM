f175c8c69182400c6c2234478f6d33da01d74f5a
kelvinkyy <kelvinkyy03@gmail.com>
2023-10-18 00:19:43 +0800

    Login Window Update, Clinic Homepage Created

--- src/ClinicHomepage.py
+++ src/ClinicHomepage.py
@@ -5,22 +5,19 @@ from PyQt5.QtGui import QFont, QPixmap, QIcon
 from PyQt5.QtWidgets import QMainWindow, QWidget, QLabel, QPushButton, QApplication
 from PyQt5 import QtWidgets
 
-from PatientClinicsNearbyWindow import PatientClinicsNearbyWindow
 
-
-class PatientHomepage(QMainWindow):
-    def __init__(self):
+class ClinicHomepage(QMainWindow):
+    def __init__(self, clinic):
         super().__init__()
+        self.clinic = clinic
         self.setWindowTitle("Homepage")
         self.setFixedWidth(1280)
         self.setFixedHeight(720)
 
         self.setupUi(self)
 
-    def goToClinicsNearby(self):
-        self.nearbyClinicWindow = PatientClinicsNearbyWindow()
-        self.nearbyClinicWindow.show()
-        self.close()
+    def goToManageSchedule(self):
+        pass
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("Homepage")
@@ -29,60 +26,59 @@ class PatientHomepage(QMainWindow):
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
 
-        self.clinicNearbyButton = QPushButton(self.centralwidget)
-        self.clinicNearbyButton.setGeometry(QRect(150, 200, 400, 100))
+        self.manageScheduleButton = QPushButton(self.centralwidget)
+        self.manageScheduleButton.setGeometry(QRect(150, 200, 400, 100))
         font = QFont()
         font.setFamily("Arial")
         font.setPointSize(20)
         font.setBold(False)
         font.setWeight(50)
-        self.clinicNearbyButton.setFont(font)
-        self.clinicNearbyButton.setObjectName("schedule")
-        self.clinicNearbyButton.setText("Clinics Nearby")
-        self.clinicNearbyButton.clicked.connect(self.goToClinicsNearby)
+        self.manageScheduleButton.setFont(font)
+        self.manageScheduleButton.setText("Manage Schedule")
+        self.manageScheduleButton.clicked.connect(self.goToManageSchedule)
 
-        self.clinicNearbyLabel = QLabel(self.centralwidget)
-        self.clinicNearbyLabel.setGeometry(QRect(170, 225, 50, 50))
-        self.clinicNearbyLabel.setFrameShape(QtWidgets.QFrame.Box)
+        self.manageScheduleLabel = QLabel(self.centralwidget)
+        self.manageScheduleLabel.setGeometry(QRect(170, 225, 50, 50))
+        self.manageScheduleLabel.setFrameShape(QtWidgets.QFrame.Box)
         filepath = os.path.join(CURRENT_DIRECTORY, "resources\\logo-placeholder-image.png")
-        self.clinicNearbyIcon = QPixmap(filepath)
-        self.clinicNearbyIcon = self.clinicNearbyIcon.scaled(50, 50)
-        self.clinicNearbyLabel.setPixmap(self.clinicNearbyIcon)
+        self.manageScheduleIcon = QPixmap(filepath)
+        self.manageScheduleIcon = self.manageScheduleIcon.scaled(50, 50)
+        self.manageScheduleLabel.setPixmap(self.manageScheduleIcon)
 
 
-        self.myPrescriptionButton = QPushButton(self.centralwidget)
-        self.myPrescriptionButton.setGeometry(QRect(700, 200, 400, 100))
+        self.doctorListButton = QPushButton(self.centralwidget)
+        self.doctorListButton.setGeometry(QRect(700, 200, 400, 100))
         font = QFont()
         font.setFamily("Arial")
         font.setPointSize(20)
-        self.myPrescriptionButton.setFont(font)
-        self.myPrescriptionButton.setLayoutDirection(Qt.LeftToRight)
-        self.myPrescriptionButton.setText("My Prescription")
+        self.doctorListButton.setFont(font)
+        self.doctorListButton.setLayoutDirection(Qt.LeftToRight)
+        self.doctorListButton.setText("Doctor List")
 
-        self.myPrescriptionLabel = QLabel(self.centralwidget)
-        self.myPrescriptionLabel.setGeometry(QRect(720, 225, 50, 50))
-        self.myPrescriptionLabel.setFrameShape(QtWidgets.QFrame.Box)
+        self.doctorListLabel = QLabel(self.centralwidget)
+        self.doctorListLabel.setGeometry(QRect(720, 225, 50, 50))
+        self.doctorListLabel.setFrameShape(QtWidgets.QFrame.Box)
         filepath = os.path.join(CURRENT_DIRECTORY, "resources\\logo-placeholder-image.png")
-        self.myPrescriptionIcon = QPixmap(filepath)
-        self.myPrescriptionIcon = self.myPrescriptionIcon.scaled(50, 50)
-        self.myPrescriptionLabel.setPixmap(self.myPrescriptionIcon)
+        self.doctorListIcon = QPixmap(filepath)
+        self.doctorListIcon = self.doctorListIcon.scaled(50, 50)
+        self.doctorListLabel.setPixmap(self.doctorListIcon)
 
 
-        self.myAppointmentsButton = QPushButton(self.centralwidget)
-        self.myAppointmentsButton.setGeometry(QRect(150, 400, 400, 100))
+        self.requestReviewButton = QPushButton(self.centralwidget)
+        self.requestReviewButton.setGeometry(QRect(150, 400, 400, 100))
         font = QFont()
         font.setFamily("Arial")
         font.setPointSize(18)
-        self.myAppointmentsButton.setFont(font)
-        self.myAppointmentsButton.setText("My Appointments")
+        self.requestReviewButton.setFont(font)
+        self.requestReviewButton.setText("Request Review")
 
-        self.myAppointmentsLabel = QLabel(self.centralwidget)
-        self.myAppointmentsLabel.setGeometry(QRect(175, 425, 50, 50))
-        self.myAppointmentsLabel.setFrameShape(QtWidgets.QFrame.Box)
+        self.requestReviewLabel = QLabel(self.centralwidget)
+        self.requestReviewLabel.setGeometry(QRect(175, 425, 50, 50))
+        self.requestReviewLabel.setFrameShape(QtWidgets.QFrame.Box)
         filepath = os.path.join(CURRENT_DIRECTORY, "resources\\logo-placeholder-image.png")
-        self.myAppointmentsIcon = QPixmap(filepath)
-        self.myAppointmentsIcon = self.myAppointmentsIcon.scaled(50, 50)
-        self.myAppointmentsLabel.setPixmap(self.myAppointmentsIcon)
+        self.requestReviewIcon = QPixmap(filepath)
+        self.requestReviewIcon = self.requestReviewIcon.scaled(50, 50)
+        self.requestReviewLabel.setPixmap(self.requestReviewIcon)
 
 
         self.topLeftLogo = QLabel(self.centralwidget)
@@ -102,7 +98,7 @@ class PatientHomepage(QMainWindow):
         font.setWeight(75)
         self.homepageTitle.setFont(font)
         self.homepageTitle.setFrameShape(QtWidgets.QFrame.Box)
-        self.homepageTitle.setText("Welcome! [name]")
+        self.homepageTitle.setText(self.clinic.getClinicName())
         self.homepageTitle.setAlignment(Qt.AlignCenter)
 
         self.myAccountButton = QPushButton(self.centralwidget)
@@ -123,4 +119,3 @@ class PatientHomepage(QMainWindow):
         MainWindow.setCentralWidget(self.centralwidget)
 
         QMetaObject.connectSlotsByName(MainWindow)
-
