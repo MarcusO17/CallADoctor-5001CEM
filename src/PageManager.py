37cbf89de9e0c56c49a809aabf199e4caa3e166c
kelvinkyy <kelvinkyy03@gmail.com>
2023-10-21 14:18:20 +0800

    Page Manager Smoother Transition

--- src/PageManager.py
+++ src/PageManager.py
@@ -11,15 +11,14 @@ class PageManager():
             self.stack.append(pageWidget)
             pageWidget.show()
         else:
-            self.stack[len(self.stack)-1].hide()
             self.stack.append(pageWidget)
             pageWidget.show()
-        # still need to hide the current page
+            self.stack[len(self.stack)-2].hide()
 
     def goBack(self):
         toBeRemovedPage = self.stack.pop()
-        toBeRemovedPage.close()
         self.stack[len(self.stack)-1].show()
+        toBeRemovedPage.close()
 
     def size(self):
         return len(self.stack)
