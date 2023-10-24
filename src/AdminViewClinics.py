74b6fb2f7bab080e43506c2e98c236be4bc85778
MaazPotrick <maaz.potrick1@gmail.com>
2023-10-24 15:45:38 +0800

    Admin Clinic View Window created. Need to make uncomment code, when next page made.

---
+++ src/AdminViewClinics.py
@@ -0,0 +1,127 @@
+import os
+import sys
+from PyQt5.QtCore import Qt, QRect, QMetaObject, QSize
+from PyQt5.QtGui import QFont, QPixmap, QIcon
+from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QLabel, QPushButton, QHBoxLayout, QApplication, \
+    QScrollArea, QSizePolicy
+from PyQt5 import QtWidgets
+# from .AdminClinicDetails import AdminClinicDetailsWindow
+from .model.Clinic import Clinic
+from .model.ClinicRepo import ClinicRepository
+from .PageManager import PageManager
+
+
+
+class AdminViewClinicsWindow(QMainWindow):
+    def __init__(self, clinic):
+        super().__init__()
+        self.clinic = clinic
+        self.pageManager = PageManager()
+        self.setWindowTitle("View Clinics Under CaD")
+        self.setFixedWidth(1280)
+        self.setFixedHeight(720)
+        self.setupUi(self)
+
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("AdminViewClinics")
+        CURRENT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+
+        # this is the header (logo, title, my back button
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+
+        # header (probably reused in most files)
+        self.topLeftLogo = QLabel(self.centralwidget)
+        self.topLeftLogo.setFrameShape(QtWidgets.QFrame.Box)
+        self.topLeftLogo.setGeometry(QRect(20, 10, 60, 60))
+
+        filepath = os.path.join(CURRENT_DIRECTORY, "resources\\logo-placeholder-image.png")
+        self.topLeftLogoIcon = QPixmap(filepath)
+        self.topLeftLogoIcon = self.topLeftLogoIcon.scaled(60, 60)
+        self.topLeftLogo.setPixmap(self.topLeftLogoIcon)
+
+        self.headerTitle = QLabel(self.centralwidget)
+        font = QFont()
+        font.setFamily("Arial")
+        font.setPointSize(28)
+        font.setBold(True)
+        font.setWeight(75)
+        self.headerTitle.setFont(font)
+        self.headerTitle.setText("Clinics Under CaD")
+        self.headerTitle.setFrameShape(QtWidgets.QFrame.Box)
+        self.headerTitle.setGeometry(QRect(200, 40, 800, 70))
+        self.headerTitle.setAlignment(Qt.AlignCenter)
+        self.headerTitle.setStyleSheet("margin-left: 20px; margin-right: 20px")
+
+        self.myAccountButton = QPushButton(self.centralwidget)
+        self.myAccountButton.setFixedSize(70,70)
+        self.myAccountButton.setGeometry(QRect(1050, 40, 70, 70))
+        filepath = os.path.join(CURRENT_DIRECTORY, "resources\\logo-placeholder-image.png")
+        self.myAccountIcon = QIcon(filepath)
+        self.myAccountButton.setIconSize(QSize(70, 70))
+        self.myAccountButton.setIcon(self.myAccountIcon)
+
+        # Push Button 5 (Log Out)
+        self.backButton = QPushButton(self.centralwidget)
+        self.backButton.setFixedSize(70, 70)
+        self.backButton.setGeometry(QRect(1150, 40, 70, 70))
+        filepath = os.path.join(CURRENT_DIRECTORY, "resources\\backbutton.png")
+        self.backIcon = QIcon(filepath)
+        self.backButton.setIconSize(QSize(70, 70))
+        self.backButton.setIcon(self.backIcon)
+        self.backButton.clicked.connect(self.backButtonFunction)
+
+        buttonContainer = QWidget()
+        button_layout = QVBoxLayout(buttonContainer)
+        buttonContainer.setContentsMargins(20,20,20,20)
+        boxScrollArea = QScrollArea()
+        boxScrollArea.setWidgetResizable(True)
+        boxScrollArea.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+
+        #Get Clinics
+        clinicList = ClinicRepository.getClinicList()
+
+        buttonFont = QFont()
+        buttonFont.setFamily("Arial")
+        buttonFont.setPointSize(28)
+        buttonFont.setBold(True)
+        buttonFont.setWeight(75)
+
+        #Inserting  All the Clinics 
+        for count, clinic in enumerate(clinicList):
+            self.clinicButton = QPushButton()
+            self.clinicButton.setText(clinic.getClinicID() + " - " + clinic.getClinicName())
+            self.clinicButton.setText(clinic.getClinicID() + " - " + clinic.getClinicName())
+            self.clinicButton.setFont(buttonFont)
+            self.clinicButton.setFixedSize(QSize(900,150))
+            # self.clinicButton.clicked.connect(lambda checked, clinic=clinic: self.clinicButtonFunction(clinic, self.clinic))
+            # buttonContainer.layout().addWidget(self.clinicButton)
+
+        spacer = QWidget()
+        spacer.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Expanding)
+        buttonContainer.layout().addWidget(spacer)
+
+        boxScrollArea.setWidget(buttonContainer)
+        boxScrollArea.setFixedSize(1000,500)
+        topSpacer = QWidget()
+        topSpacer.setFixedHeight(150)
+        mainLayout = QVBoxLayout()
+        mainLayout.addWidget(topSpacer)
+        mainLayout.addWidget(boxScrollArea)
+        mainLayout.setAlignment(Qt.AlignHCenter)
+
+        self.centralwidget.setLayout(mainLayout)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+
+    # def clinicButtonFunction(self, clinic, patient):
+        # update the clinic details page here according to button click
+	# previously had patient with clinic as parameter, removed it
+
+        # self.adminClinicDetails = AdminClinicDetailsWindow(clinic)
+        # self.pageManager.add(self.adminClinicDetails)
+
+    def backButtonFunction(self):
+        self.pageManager.goBack()
