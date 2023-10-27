3426b0c2d33932f4ad0dda16470625d937b190a5
MarcusO17 <marcusonggeye@gmail.com>
2023-10-27 21:39:18 +0800

    added map feature

--- src/helper/geocoder.py
+++ src/helper/geocoder.py
@@ -13,10 +13,19 @@ class GeoHelper:
             return(None,None)
         lat=location.latitude
         lon=location.longitude
-        return(lat,lon)
+        return[lat,lon]
     
 
-
-
-
+    def showMap(self,coordinates):
+        map = folium.Map(location=coordinates,zoom_start=11.6)
+        return map
+        
     
+    def addMarker(self,map,location,text):
+        folium.Marker(
+            location=location,
+            popup=text,
+            icon=folium.Icon(icon='star')
+        ).add_to(map)
+
+
