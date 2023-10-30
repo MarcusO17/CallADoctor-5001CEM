09f15a9081e101d906c3f224b098649cd664adf9
MarcusO17 <marcusonggeye@gmail.com>
2023-10-30 19:27:36 +0800

    function to generate graphs + setup.py changes

---
+++ src/model/graphs.py
@@ -0,0 +1,21 @@
+import plotly_express as px
+import matplotlib.pyplot as plt
+import requests
+import pandas as pd
+
+
+class graphGen:
+    def generateGraph():
+        response = requests.get('http://127.0.0.1:5000/graph/users').json()
+
+        df = pd.DataFrame(response)
+        df= df.sort_values(by="dates")
+        df.index = df.index.astype(int)
+        fig = px.line(df, x="dates", y="count", title='Appointments by Day').update_layout(
+                    xaxis_title="Date", yaxis_title="Appointments"
+                )
+        return fig.to_image(format="png", width=600, height=350, scale=2)
+    
+        """
+        plt.plot(df['dates'],df['count'])
+        plt.show()"""
